efb178207a1fb677393b36ac5a80bfd6aaf2af78
Margaret Scott <eudaimonious@gmail.com>
2013-06-20 12:06:11 -0400

    First commit, new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learntodo::Application.config.secret_token = '23772ed71112a622334951cc18ed7fc2c48578a539fcdd4bb0055cdd4bd2a52f1fd85fce0988b6c9df110924a6a2edad089765f436cfa6c51b8182fb143bb0b8'
